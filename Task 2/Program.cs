db032a3e5f06b082a90f5e44c37406d19354cf9c
ravindudilhara99 <rdrajapaksha67@gmail.com>
2025-03-25 12:40:36 +0530

    Re code All and Correct code

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -4,16 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Abstract base class Vehicle
-public abstract class Vehicle
+namespace Task02Abstract
 {
-    //property for Make
-    public string Make { get; set; }
 
-    //constructor to initialize Make 
-    public Vehicle(string make)
+    // syntax for creating an abstract class
+    // access_modifier Keyword_abstract Keyword_class class_name
+    public abstract class Vehicle
     {
-        Make = make;
+        public string Make { get; }
+
+        // abstract method
+        //public abstract void StartEngine();
+
+        //constructor
+        public Vehicle(string make)
+        {
+            this.Make = make;
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public int NoofDoors;
+
+        public Car(string make, int doors) : base(make)
+        {
+            NoofDoors = doors;
+        }
+
+        /*public override void StartEngine()
+        {
+            Console.WriteLine("BRRRR.....");
+        }*/
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Ferrari", 2);
+            Console.WriteLine($"My Car is a {car1.Make} with {car1.NoofDoors} doors.");
+            Console.Read();
+        }
     }
 }
-
