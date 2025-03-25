a4c7c343287827b54bb935f4313fc5971722f96c
ravindudilhara99 <rdrajapaksha67@gmail.com>
2025-03-25 11:59:07 +0530

    Make Abstruction

---
+++ Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Abstract base class Vehicle
+public abstract class Vehicle
+{
+    //property for Make
+    public string Make { get; set; }
+
+    //constructor to initialize Make 
+    public Vehicle(string make)
+    {
+        Make = make;
+    }
+}
+
